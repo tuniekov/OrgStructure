26b7b7c867c326059e523fe99bbacc542e16ff43
tuniekov <tuniekov@gmail.com>
2025-07-15 03:21:40 +0700

    111

---
+++ example.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="orgstructure">
+    <UniTreePanel :treetabs="treetabs"/>
+  </div>
+</template>
+
+<script setup>
+  import { UniTreePanel } from 'pvtables/dist/pvtables'
+  import { ref } from 'vue'
+
+  const treetabs = ref({
+    osTree:{ //Имя таба
+      type:'tree', // тип таба
+      title: 'Структура организации', // подпись таба
+      table: 'osTree', // Таблица дерева заданная в _build\configs\gtsapipackages.js
+      dragable: true // перемещать узлы
+    },
+  });
+</script>
+
+<style>
+ 
+</style>
