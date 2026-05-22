ae2de81b468762144007bc9b98c5b355e5e74d5e
tuniekov <tuniekov@gmail.com>
2026-05-22 18:29:12 +0700

    osEmployee: расписание для СКУД (track_schedule, work_time_from/to, has_lunch)

--- _build/configs/gtsapipackages.js
+++ _build/configs/gtsapipackages.js
@@ -429,7 +429,7 @@ export default {
                 table:'osEmployee', //Название таблицы
                 //class:'osEmployee', //Класс MODX таблицы базы данных. Если совпадает с table писать не обязательно.
                 autocomplete_field:'os_employee_id', //Если задано то при определении полей таблицы автоматически узнает поле autocomplect
-                version:4, //при изменении в файле надо обновлять версию, чтобы изменения применились при установке.
+                version:5, //при изменении в файле надо обновлять версию, чтобы изменения применились при установке.
                 type: 1, //тип таблицы: 1 - таблица PVTables, 2 - таблица JSON, 3 - дерево UniTree
                 authenticated:true, //доступ к таблице только аутентифицированным пользователям
                 groups:'', //Можно определить группы пользователей которые будут иметь доступ к таблицам.
@@ -471,6 +471,22 @@ export default {
                             label:'Должность',
                             type:'autocomplete',
                             table:'osPost',
+                        },
+                        track_schedule:{
+                            label:'Учитывать расписание',
+                            type:'boolean',
+                        },
+                        work_time_from:{
+                            label:'Начало',
+                            type:'text',
+                        },
+                        work_time_to:{
+                            label:'Конец',
+                            type:'text',
+                        },
+                        has_lunch:{
+                            label:'Обед',
+                            type:'boolean',
                         },
                     }
                 }
